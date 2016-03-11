b669bb444d377978633db50b52823a90071133e3
Chris Brackert <cbrackert@gmail.com>
2016-03-10 16:34:25 -0800

    Upgrade blocks for nio 2.0

--- system_metrics_block.py
+++ system_metrics_block.py
@@ -6,15 +6,15 @@ import subprocess
 import platform
 from datetime import datetime
 import pickle
-from nio.common.block.base import Block
-from nio.common.signal.base import Signal
-from nio.common.command import command
-from nio.common.discovery import Discoverable, DiscoverableType
-from nio.metadata.properties.object import ObjectProperty
-from nio.metadata.properties.holder import PropertyHolder
-from nio.metadata.properties.bool import BoolProperty
-from nio.metadata.properties.timedelta import TimeDeltaProperty
-from nio.metadata.properties.version import VersionProperty
+from nio.block.base import Block
+from nio.signal.base import Signal
+from nio.command import command
+from nio.util.discovery import discoverable
+from nio.properties.object import ObjectProperty
+from nio.properties.holder import PropertyHolder
+from nio.properties.bool import BoolProperty
+from nio.properties.timedelta import TimeDeltaProperty
+from nio.properties.version import VersionProperty
 from nio.modules.scheduler import Job
 from .sensors import Sensors
 
@@ -35,39 +35,29 @@ class Menu(PropertyHolder):
     sensors = BoolProperty(title='Sensors', default=False)
 
     # a fairly long list in the general case. not included by default
-    pids = BoolProperty(title='Process Identifiers')
+    pids = BoolProperty(title='Process Identifiers', default=False)
 
     # this requires superuser priviledges and dumps a whole ton of
     # data. Maybe unnecessary? Not included by default
-    skt_conns = BoolProperty(title='Socket Connections')
+    skt_conns = BoolProperty(title='Socket Connections', default=False)
 
 
 @command('platform')
 @command('timestamp')
 @command('cpu')
 @command('report')
-@Discoverable(DiscoverableType.block)
-class Metrics(Block):
+@discoverable
+class SystemMetrics(Block):
 
     version = VersionProperty('0.1.0', min_version='0.1.0')
-    menu = ObjectProperty(Menu, title='Menu')
-    interval = TimeDeltaProperty(title='Interval')
+    menu = ObjectProperty(Menu, title='Menu', default=Menu())
 
     def __init__(self):
         super().__init__()
-        self._metrics_job = None
         self._retry_count = 0
 
-    def start(self):
-        self._metrics_job = Job(
-            self._collect_and_notify,
-            self.interval,
-            True
-        )
-
-    def stop(self):
-        if self._metrics_job is not None:
-            self._metrics_job.cancel()
+    def process_signals(self, signals):
+        self._collect_and_notify()
 
     def timestamp(self):
         '''returns the current system timestamp'''
@@ -131,7 +121,7 @@ class Metrics(Block):
 
         try:
             # CPU usage statistics
-            if self.menu.cpu_perc:
+            if self.menu().cpu_perc():
                 base = 'cpu_percentage'
                 fields = ['overall', 'per_cpu']
                 for idx, f in enumerate(fields):
@@ -139,53 +129,53 @@ class Metrics(Block):
                     result["{0}_{1}".format(base,f)] = data
 
             # Virtual memory usage
-            if self.menu.virtual_mem:
+            if self.menu().virtual_mem():
                 self._collect_results('virtual_memory', result)
 
             # Swap memory usage
-            if self.menu.swap_mem:
+            if self.menu().swap_mem():
                 self._collect_results('swap_memory', result)
                 # result['swap_memory'] = psutil.swap_memory()._asdict()
 
             # Disk usage
-            if self.menu.disk_usage:
+            if self.menu().disk_usage():
                 self._collect_results('disk_usage', result, ['/'])
 
             # Disk I/O statistics
-            if self.menu.disk_io_ct:
+            if self.menu().disk_io_ct():
                 self._collect_results('disk_io_counters', result)
 
             # Net I/O statistics
-            if self.menu.net_io_ct:
+            if self.menu().net_io_ct():
                 self._collect_results('net_io_counters', result)
 
             # Sensors statistics - disabled by default.
             # Requires [lm-sensors](http://www.lm-sensors.org/)
-            if self.menu.sensors:
+            if self.menu().sensors():
                 self._collect_sensors_results(result)
 
             # Process IDs - this dumps a big list and is disabled by default
-            if self.menu.pids:
+            if self.menu().pids():
                 result['process_identifiers'] = psutil.pids()
 
             # Socket Connections - dumps a big list and requires superuser
             # also disabled by default. Dumps a list of dictionaries; this
             # approach won't look very nice in the database, but is generally
             # quite long and not usually used.
-            if self.menu.skt_conns:
+            if self.menu().skt_conns():
                 result['network_connections'] = \
                     [self.native_dict(skt._asdict()) \
                      for skt in psutil.net_connections()]
 
         except Exception as e:
-            self._logger.error(
+            self.logger.error(
                 "While processing system metrics: {0}".format(str(e))
             )
             if self._retry_count < RETRY_LIMIT:
                 self._retry_count += 1
                 return self._collect_stats()
             else:
-                self._logger.error(
+                self.logger.error(
                     "System report failed {0} times, aborting...".format(
                         RETRY_LIMIT)
                 )
@@ -205,21 +195,21 @@ class Metrics(Block):
 
     def _collect_sensors_results(self, result):
         try:
-            self._logger.debug('Reading sensors')
+            self.logger.debug('Reading sensors')
             sensors = Sensors()
             sensors.read(result)
         except subprocess.CalledProcessError:
-            self._logger.exception('Looks like lm-sensors is not installed')
+            self.logger.exception('Looks like lm-sensors is not installed')
         except:
-            self._logger.exception('Unexpected failure reading sensors')
+            self.logger.exception('Unexpected failure reading sensors')
 
     def _subprocess_command(self, command):
         out = None
         try:
             out = subprocess.check_output(command, shell=True).strip().decode()
-            self._logger.debug('Command output: {}: {}'.format(command, out))
+            self.logger.debug('Command output: {}: {}'.format(command, out))
         except:
-            self._logger.exception(
+            self.logger.exception(
                 'Failed running subprocess command: {}'.format(command))
         return out
 
